8d16cd29a48426a5b459d00b5d89890b9ae65ecb
Mr.Kittikhun Klinnarai <71427085+kittikhun1910@users.noreply.github.com>
2022-03-10 14:49:12 +0700

    add checkpass from user in database

--- myapp/auth.py
+++ myapp/auth.py
@@ -19,15 +19,14 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        user = User.query.filter_by(email=email).frist()
+        user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Account created!', category='success')
+                flash('Logged in successfully!', category='success')
             else:
-                flash('Passwords don\'t match.', category='error')
+                flash('Incorrect password , try again', category='error')
         else:
-            flash('Email must be greater than 3 characters.', category='error')
-
+            flash('Email does not exist.', category='error')
 
     return render_template("login.html")
 
