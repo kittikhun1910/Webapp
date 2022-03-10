42a73440131140c4d718121d893e5851a38e5b30
Mr.Kittikhun Klinnarai <71427085+kittikhun1910@users.noreply.github.com>
2022-03-10 14:24:05 +0700

    add auth.route path in file

---
+++ myapp/auth.py
@@ -0,0 +1,46 @@
+from flask import *
+from models import User
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/')
+def home():
+    return render_template("home.html")
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+    
+    return render_template("login.html")
+
+
+@auth.route('/logout')
+def logout():
+    return "<h1>Logout</h1>"
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("signup.html")
+
+
